c114341af2e98f665d986427b73f9300f6c636c7
Dzhangir Bayandarov <dbayandarov@gmail.com>
2023-04-27 17:26:22 -0700

    add boilerplate floor,date,time pickers

---
+++ scripts/floorPicker.js
@@ -0,0 +1,29 @@
+const floors = [{
+        name: "1st-floor",
+        image: "../maps/1st-floor.png",
+    },
+    {
+        name: "2nd-floor",
+        image: "../maps/2nd-floor.png",
+    },
+    {
+        name: "3rd-floor",
+        image: "../maps/3rd-floor.png",
+    },
+    {
+        name: "4th-floor",
+        image: "../maps/4th-floor.png",
+    },
+    {
+        name: "Annex-1st",
+        image: "../maps/Annex-1st.png",
+    },
+    {
+        name: "Annex-2nd",
+        image: "../maps/Annex-2nd.png",
+    },
+    {
+        name: "Annex-lower-level",
+        image: "../maps/Annex-lower-level.png",
+    },
+]
